e0328c6256443810217c64b4f3650678af055841
Lozing <lozingyt@gmail.com>
2024-02-15 15:44:29 +0000

    linked database to webapp

--- ecm2434_coursework/webapp/models.py
+++ ecm2434_coursework/webapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here. (database)
+# Create your models here. (database) 
+
+# Run this to update the database scheme
+# $ python manage.py makemigrations polls
