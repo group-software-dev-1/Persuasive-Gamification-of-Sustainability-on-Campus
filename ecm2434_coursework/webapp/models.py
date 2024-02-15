d3c2d7a372de68e9951bd6be74f380b9711a1852
Lozing <lozingyt@gmail.com>
2024-02-15 15:45:59 +0000

    more database directions

--- ecm2434_coursework/webapp/models.py
+++ ecm2434_coursework/webapp/models.py
@@ -4,3 +4,5 @@ from django.db import models
 
 # Run this to update the database scheme
 # $ python manage.py makemigrations polls
+# $ python manage.py sqlmigrate polls 0001
+# $ python manage.py migrate
