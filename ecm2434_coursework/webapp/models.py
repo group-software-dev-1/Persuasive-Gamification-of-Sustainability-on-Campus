4f1ba8cd5ea547572f8c1e640a6d7cdf76f8c21c
JonathanRut <jmrutland@gmail.com>
2024-02-19 12:57:51 +0000

    Restart on project

--- ecm2434_coursework/webapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here. (database) 
-
-# Run this to update the database scheme
-# $ python manage.py makemigrations polls
-# $ python manage.py sqlmigrate polls 0001
-# $ python manage.py migrate
