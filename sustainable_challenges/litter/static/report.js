3c4d66822f443148c4f0426e3fd6ac1463f788e3
Lozing <lozingyt@gmail.com>
2024-02-20 17:05:57 +0000

    logic to get the users location

---
+++ sustainable_challenges/litter/static/report.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", (event) => {
+    function success(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        document.getElementById('lat_field').value = latitude;
+        document.getElementById('lon_field').value = longitude;
+        document.getElementById('datetime').value = new Date();
+
+        console.log(latitude);
+        console.log(longitude);
+    }
+
+    function error() {
+        console.log("Unable to retrieve your location");
+    }
+
+    if (!navigator.geolocation) {
+        console.log("Location not supported by your browser");
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+});
