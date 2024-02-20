67b69f75f6092d7ad1690142640dc94d186816a5
Lozing <lozingyt@gmail.com>
2024-02-20 17:22:23 +0000

    changed to new auth user model

--- sustainable_challenges/litter/models.py
+++ sustainable_challenges/litter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
 class LitterInstance(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
