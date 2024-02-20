fa2eba8dd17929ff0b1ef9979d96966a27bc1b0e
Lozing <lozingyt@gmail.com>
2024-02-20 16:25:40 +0000

    Made litter instance model

--- sustainable_challenges/litter/models.py
+++ sustainable_challenges/litter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class LitterInstance(models.Model):
+    #submitter = models.ForeignKey(User, on_delete=models.CASCADE)
+    lat = models.DecimalField(default=0, decimal_places=7, max_digits=10)
+    lon = models.DecimalField(default=0, decimal_places=7, max_digits=10)
+    img = models.ImageField(null=True, blank=True)
+    datetime = models.DateTimeField("Date submitted")
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"User: {self.submitter}, {self.lat}, {self.lon} @ {self.datetime}"
