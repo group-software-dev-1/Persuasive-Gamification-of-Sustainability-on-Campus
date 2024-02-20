677402a121c6a6948532e1bb12b6db49f6bb8adb
Lozing <lozingyt@gmail.com>
2024-02-20 16:47:29 +0000

    modified to user django user model

--- sustainable_challenges/litter/models.py
+++ sustainable_challenges/litter/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class LitterInstance(models.Model):
-    #submitter = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     lat = models.DecimalField(default=0, decimal_places=7, max_digits=10)
     lon = models.DecimalField(default=0, decimal_places=7, max_digits=10)
     img = models.ImageField(null=True, blank=True)
@@ -10,4 +10,4 @@ class LitterInstance(models.Model):
     approved = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"User: {self.submitter}, {self.lat}, {self.lon} @ {self.datetime}"
+        return f"User: {self.user}, {self.lat}, {self.lon} @ {self.datetime}"
