0f97bd18721b2e24947c4d3c2bd1a31b5bd2c5b2
Lozing <lozingyt@gmail.com>
2024-02-20 17:28:20 +0000

    updated place for litter instance images

--- sustainable_challenges/litter/models.py
+++ sustainable_challenges/litter/models.py
@@ -6,7 +6,7 @@ class LitterInstance(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     lat = models.DecimalField(default=0, decimal_places=7, max_digits=10)
     lon = models.DecimalField(default=0, decimal_places=7, max_digits=10)
-    img = models.ImageField(null=True, blank=True)
+    img = models.ImageField(null=True, blank=True, upload_to='images/litter/')
     datetime = models.DateTimeField("Date submitted")
     approved = models.BooleanField(default=False)
 
