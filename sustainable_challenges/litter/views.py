062b1a05ece73284d17834c86385d39f2d3c7f9a
Lozing <lozingyt@gmail.com>
2024-02-21 17:36:27 +0000

    added more to the context to help identify if the user should be accessing this page

--- sustainable_challenges/litter/views.py
+++ sustainable_challenges/litter/views.py
@@ -9,17 +9,25 @@ from django.utils import timezone
 from .forms import InstanceForm
 from json import dumps
 
-class LatestView(generic.ListView):
-    template_name = "litter/latest.html"
-    context_object_name = "latest_instance_list"
+# class LatestView(generic.ListView):
+#     template_name = "litter/latest.html"
+#     context_object_name = "latest_instance_list"
 
-    def get_queryset(self) -> QuerySet[Any]:
-        valid_instances = LitterInstance.objects.filter(approved=False)
-        return valid_instances.order_by("-datetime")[:5]
+#     def get_queryset(self) -> QuerySet[Any]:
+#         valid_instances = LitterInstance.objects.filter(approved=False)
+#         return valid_instances.order_by("-datetime")[:5]
+    
+def latest(request):
+    valid_instances = LitterInstance.objects.filter(approved=False).order_by("-datetime")
+    return render(request, "litter/latest.html", {"latest_instance_list": valid_instances, 
+                                                  "is_staff": request.user.is_staff})
+
     
 def instance(request, instance_id):
     _instance = get_object_or_404(LitterInstance, pk=instance_id)
-    return render(request, "litter/instance.html", {"instance": _instance})
+    return render(request, "litter/instance.html", {"instance": _instance, 
+                                                    "requester_id": request.user.id, 
+                                                    "is_staff": request.user.is_staff})
 
 def report(request):
     submitted = False
@@ -41,7 +49,9 @@ def report(request):
             submitted = True
             id = request.GET['id']
 
-    return render(request, 'litter/report.html', {'form': form, 'submitted': submitted, 'id': id})
+    return render(request, 'litter/report.html', {'form': form, 
+                                                  'submitted': submitted, 
+                                                  'id': id})
 
 
 def heatmap(request):
