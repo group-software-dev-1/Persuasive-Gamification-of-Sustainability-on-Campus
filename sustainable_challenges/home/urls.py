7bb7b1039a72c909af124ba275e23f086877957d
JonathanRut <jmrutland@gmail.com>
2024-02-22 12:58:42 +0000

    Added paths to home page

--- sustainable_challenges/home/urls.py
+++ sustainable_challenges/home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from . import views
 
-app_name="game"
+app_name="home"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login, name="login")
