bb0d4dc2abf6b7904adda7f7d61d586f16a1d67e
Lozing <lozingyt@gmail.com>
2024-02-23 13:53:46 +0000

    added explicit verbose names

--- sustainable_challenges/litter/models.py
+++ sustainable_challenges/litter/models.py
@@ -3,12 +3,12 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 class LitterInstance(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    lat = models.DecimalField(default=0, decimal_places=7, max_digits=10)
-    lon = models.DecimalField(default=0, decimal_places=7, max_digits=10)
-    img = models.ImageField(null=True, blank=True, upload_to='images/litter/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="User")
+    lat = models.DecimalField("Latitude", default=0, decimal_places=7, max_digits=10)
+    lon = models.DecimalField("Longitude", default=0, decimal_places=7, max_digits=10)
+    img = models.ImageField("Image", null=True, blank=True, upload_to='images/litter/')
     datetime = models.DateTimeField("Date submitted")
-    approved = models.IntegerField(default=0)
+    approved = models.IntegerField("Approval status", default=0)
 
     def __str__(self):
         return f"User: {self.user}, {self.lat}, {self.lon} @ {self.datetime}"
