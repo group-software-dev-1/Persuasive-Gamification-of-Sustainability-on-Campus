12917b7aee73ebc5b98aef26b946c98327d6d8a5
JonathanRut <jmrutland@gmail.com>
2024-02-24 23:42:16 +0000

    Added basic shop

--- sustainable_challenges/game/urls.py
+++ sustainable_challenges/game/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 app_name="game"
 urlpatterns = [
-    path("", views.game, name="game")
+    path("", views.game, name="game"),
+    path("/shop", views.shop, name="shop")
 ]
--- sustainable_challenges/game/views.py
+++ sustainable_challenges/game/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def game(request):
     context = {"level_numbers": list(range(1,11))}
     return render(request, "game/game.html", context)
+
+def shop(request):
+    return render(request, "game/shop.html")
