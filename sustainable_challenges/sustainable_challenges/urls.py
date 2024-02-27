476fe07aca4b5aa1d8649b7b62027a55ed73d323
Jacob <jw.braidley@gmail.com>
2024-02-27 14:48:44 +0000

    landing page content + some navbar functionality

--- sustainable_challenges/sustainable_challenges/urls.py
+++ sustainable_challenges/sustainable_challenges/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('register/', include('authuser.urls')),
-    path('game', include('game.urls'))
+    path('game/', include('game.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
