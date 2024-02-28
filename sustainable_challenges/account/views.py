963877b9b5f3c4a49cf4bd3a62f34ada1d01dbac
vr303-exeter <vr303@exeter.ac.uk>
2024-02-28 14:58:17 +0000

    Conflict fixes

--- sustainable_challenges/account/views.py
+++ sustainable_challenges/account/views.py
@@ -1,11 +1,12 @@
 # views.py
 from django.shortcuts import render
+from authuser.forms import RegistrationForm, LoginForm
 
 
 def user_account(request):
     return render(request, "user_account.html")
 
 def admin_account(request):
-    if not request.user.is_admin:
+    if not request.user.is_staff:
         return HttpResponse("Unauthorized", status=401)
-    return render(request, "admin_account")
+    return render(request, "admin_account.html")
