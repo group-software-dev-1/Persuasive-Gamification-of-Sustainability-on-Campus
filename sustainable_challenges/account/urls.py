e335f3dafb9e3b5de8d83a5a55bbecf8be8608d1
vr303-exeter <vr303@exeter.ac.uk>
2024-02-29 10:38:32 +0000

    db conflict hopefully fixed

--- sustainable_challenges/account/urls.py
+++ sustainable_challenges/account/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "account"
+# paths to app
 urlpatterns = [
     path("user_account", views.user_account, name='user_account'),
     path("admin_account", views.admin_account, name='admin_account'),
