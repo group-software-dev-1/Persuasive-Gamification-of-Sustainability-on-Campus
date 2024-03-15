e9864efcf74a9d5b06af7297bfe49f39e95c08e2
Lozing <lozingyt@gmail.com>
2024-03-15 15:03:02 +0000

    created poi webapp

---
+++ sustainable_challenges/poi/views.py
@@ -0,0 +1,11 @@
+from typing import Any
+from django.http import HttpResponseRedirect, HttpRequest, HttpResponse, HttpResponseForbidden
+from .models import LitterInstance
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+#from .forms import InstanceForm, ApproveForm, TimeRangeForm
+from json import dumps
+
+# Create your views here.
+
