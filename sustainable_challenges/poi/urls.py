cbe49823430c5d1b16bc5736abdb50690a9806aa
Lozing <lozingyt@gmail.com>
2024-03-15 17:54:10 +0000

    added info url

--- sustainable_challenges/poi/urls.py
+++ sustainable_challenges/poi/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name="poi"
 urlpatterns = [
     # path("map/", views.map, name="map"),
-    # path("info/<int:poi_id>/", views.information, name="info"),
+    path("info/<int:poi_id>/", views.information, name="info"),
     path("submit/", views.submit, name='submit'),
 ]
