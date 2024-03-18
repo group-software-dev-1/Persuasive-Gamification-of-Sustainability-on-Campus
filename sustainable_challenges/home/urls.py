87422dabb848a47767fadbc711a318e2e91edbbe
Jacob <jw.braidley@gmail.com>
2024-03-18 15:12:18 +0000

    functional about us navbar dropdown

--- sustainable_challenges/home/urls.py
+++ sustainable_challenges/home/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name="home"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("howto/", views.howto, name="howto"),
+    path("goals/", views.goals, name="goals"),
+    path("privacy/", views.privacy, name="privacy")
 ]
