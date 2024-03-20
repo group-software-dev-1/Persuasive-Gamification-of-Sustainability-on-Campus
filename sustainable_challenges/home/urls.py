649d765a4e472b0bdd51ac60abfe6657cdc26cb9
Jacob <jw.braidley@gmail.com>
2024-03-20 14:58:13 +0000

    removed how to page

--- sustainable_challenges/home/urls.py
+++ sustainable_challenges/home/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name="home"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("howto/", views.howto, name="howto"),
     path("goals/", views.goals, name="goals"),
     path("privacy/", views.privacy, name="privacy")
 ]
--- sustainable_challenges/home/views.py
+++ sustainable_challenges/home/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, '../home/index.html', {"is_staff": request.user.is_staff})
 
-def howto(request):
-    return render(request, '../home/howto.html', {"is_staff": request.user.is_staff})
-
 def goals(request):
     return render(request, '../home/goals.html', {"is_staff": request.user.is_staff})
 
