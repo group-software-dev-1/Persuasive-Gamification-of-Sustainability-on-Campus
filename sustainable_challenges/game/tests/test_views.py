d894049ddc5bf6f00145dc008a61511b83bace45
JonathanRut <jmrutland@gmail.com>
2024-03-21 09:47:51 +0000

    Merge

--- sustainable_challenges/game/tests/test_views.py
+++ sustainable_challenges/game/tests/test_views.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from game.models import *
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from game.models import Raffle
+from authuser.forms import RegistrationForm
+from authuser.models import User  
 
 class TestGame(TestCase):
 
@@ -75,3 +78,50 @@ class TestShop(TestCase):
         self.assertEqual(0, response.context[0]['points'])
         self.assertEqual({'amazon': 10000, 'john_lewis':10000, 'blackwells':50000, 'ram':50000, 'student_guild':50000, 'marketplace':50000}, response.context[0]['prize_to_points'])
         self.assertEqual(6, len(response.context[0]['completion']))
+
+class RaffleViewTestCase(TestCase):
+
+    def setUp(self):
+        user_data = {
+            'email': 'user@example.com',
+            'username': 'regularuser',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+            'first_name': 'Regular',
+            'last_name': 'User',
+        }
+        form = RegistrationForm(user_data)
+        self.assertTrue(form.is_valid())
+        self.user = form.save()
+
+        self.daily_raffle = Raffle.objects.create(name='Daily', num_participants=0, points_accumulated=0)
+        self.weekly_raffle = Raffle.objects.create(name='Weekly', num_participants=0, points_accumulated=0)
+        self.client = Client()
+
+    def test_raffle_view_loads_correctly(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('game:raffle'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_enter_daily_raffle(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('game:raffle'), {'code': 'daily', 'username': self.user.username})
+        self.assertRedirects(response, reverse('game:raffle'))
+        self.daily_raffle.refresh_from_db()
+        self.assertIn(self.user.username, self.daily_raffle.participants.split(','))
+
+    def test_enter_weekly_raffle(self):
+        self.client.force_login(self.user)
+        self.user.points += 500
+        self.user.save()
+        response = self.client.post(reverse('game:raffle'), {'code': 'weekly', 'username': self.user.username})
+        self.assertRedirects(response, reverse('game:raffle'))
+        self.weekly_raffle.refresh_from_db()
+        self.assertIn(self.user.username, self.weekly_raffle.participants.split(','))
+
+    def test_enter_weekly_raffle_no_points(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('game:raffle'), {'code': 'weekly', 'username': self.user.username})
+        self.assertRedirects(response, reverse('game:raffle'))
+        self.weekly_raffle.refresh_from_db()
+        self.assertNotIn(self.user.username, self.weekly_raffle.participants.split(','))
